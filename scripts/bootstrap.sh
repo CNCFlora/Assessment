461a76cb86a9f89bd11943bd4a80b50139a6c26c
Diogo Souza <diogo@diogok.net>
2014-02-10 15:33:44 -0200

    trying to use rbenv instead of rvm

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,21 +2,22 @@
 
 # ruby, java, git, curl and couchdb
 apt-get update
-apt-get install ruby openjdk-7-jdk curl git couchdb libgd2-noxpm tmux vim -y
+apt-get install ruby default-jre-headless curl git couchdb libgd2-noxpm tmux vim libxslt-dev libxml2-dev ruby1.9.1-dev -y
 
 cp /etc/rc.local /etc/rc.local.bkp
 sudo sed -e 's/exit/#exit/g' /etc/rc.local.bkp > /etc/rc.local
 
-# config ruby gems to https and rvm
-gem sources -r http://rubygems.org/
+# config ruby gems to https
 gem sources -r http://rubygems.org
+gem sources -r http://rubygems.org/
 gem sources -a https://rubygems.org
-curl -L https://get.rvm.io | bash -s stable
-echo "source /usr/local/rvm/scripts/rvm" >> ~/.bashrc
-source /usr/local/rvm/scripts/rvm
-rvm get stable
-rvm install $(cat /vagrant/.ruby-version) --verify-downloads 1 --max-time 200 --binary
 gem install bundler
+
+# add rbenv
+su vagrant -c 'git clone https://github.com/sstephenson/rbenv.git ~/.rbenv'
+su vagrant -c "echo 'export PATH=\"$HOME/.rbenv/bin:$PATH\"' >> ~/.bash_profile"
+su vagrant -c "echo 'eval \"$(rbenv init -)\"' >> ~/.bash_profile"
+su vagrant -c 'git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build'
 
 # initial config of app
 cd /vagrant
